b51931e67c9c35436872b696ddbefa71a8eff146
Emanuel Álvarez <Emanuel lvarez@DESKTOP-12MD67R>
2018-11-06 21:20:46 -0500

    Creada la interface y su implementacion

---
+++ NegocioVending/src/facades/IVending.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import entidades.Denominacion;
+import entidades.Producto;
+import entidades.Linea;
+
+/**
+ *
+ * @author Emanuel Álvarez
+ */
+public interface IVending {
+    
+    DTOTotal agregarLinea(Producto p_producto, int cantidad);
+    int terminarVenta();
+    DTOTotal eliminarLinea(Linea p_linea);
+    boolean nuevaVenta();
+    boolean agragarMoneda(Denominacion p_Denominacion);
+    DTOReporte reporteDiario();
+    
+}
---
+++ NegocioVending/src/facades/Vending.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import entidades.Denominacion;
+import entidades.Linea;
+import entidades.Producto;
+
+/**
+ *
+ * @author Emanuel Álvarez
+ */
+public class Vending implements IVending{
+
+    @Override
+    public DTOTotal agregarLinea(Producto p_producto, int cantidad) {
+        return null;
+    }
+
+    @Override
+    public int terminarVenta() {
+        return 0;
+    }
+
+    @Override
+    public DTOTotal eliminarLinea(Linea p_linea) {
+        return null;
+    }
+
+    @Override
+    public boolean nuevaVenta() {
+        return false;
+    }
+
+    @Override
+    public boolean agragarMoneda(Denominacion p_Denominacion) {
+        return false;
+    }
+
+    @Override
+    public DTOReporte reporteDiario() {
+        return null;
+    }
+    
+}
